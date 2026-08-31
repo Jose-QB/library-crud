24e4d4f952e2eaccc5f4b02e8e3a2acd28c65304
Jose-QB <jose-qb@hotmail.com>
2026-08-31 10:14:38 -0600

    refactor: remove security implementation from backend

--- back-end/src/main/java/com/library/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.library.entity;
-
-public enum Role {
-
-    USER,
-    ADMIN
-}
--- back-end/src/main/resources/db/migration/V2__grant_app_user_permissions.sql
+++
@@ -1,7 +0,0 @@
-GRANT SELECT, INSERT, UPDATE, DELETE
-ON TABLE library.app_user
-TO library_user;
-
-GRANT USAGE, SELECT
-ON SEQUENCE library.app_user_id_seq
-TO library_user;
